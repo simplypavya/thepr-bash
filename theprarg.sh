482652b3cc7df1db0d01271f3105bc82f3c8f2ae
PravinReddy Yalawar <simplypavya@gmail.com>
2018-07-23 15:52:28 +0530

    Initial commit

---
+++ theprarg.sh
@@ -0,0 +1,12 @@
+/#!/usr/bin/env bash
+################################################################################################################
+# Name: Pravin Yalawar
+# Title: theprarg
+# Description: This script is need two argument :
+# First Name & Last Name
+# This script displays information about captured arguments and then display the total argumenst supplied
+################################################################################################################
+echo "First Name: $1"; # Appending the first Argument
+echo "Last Name: $2"; # Appending the Second Argument
+echo "Full Name : $1 $2"; # Appending both Arguments
+echo "Total Argumenst: $#" # Ask to display total arguments supplied
---
+++ theprfirst.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+################################################################################################################
+# Name: Pravin Yalawar
+# Title: theprfirst
+# Description: This is the first script to say Hello to Cloud Automation Team v2.0
+# Also we will demonstrate New line function during this demo
+# The script will display two lines
+# Welcome to CAT 2.0 Course & Lets start the Scripting Foundation
+#
+################################################################################################################
+echo "Welcome to CAT 2.0 Course"; # display message inquotes
+echo "Lets start the Scripting Foundation"; #display message inquotes
+
+#echo -e "Welcome to CAT 2.0 Course\nLets start the Scripting Foundation"; # Display both messages in one liner
